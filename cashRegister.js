06c2f5c1131ef4337d12fcdcfdf4894b704d64f9
hattiza <quiet.iron8712@fastmail.com>
2024-05-20 13:22:15 -0700

    Add code from stretch break

--- cashRegister.js
+++ cashRegister.js
@@ -45,18 +45,87 @@ See below for an example of a cash-in-drawer array:
 
 */
 
+function fetchType(moneyRequestedName, cashRegisterState) {
+  for (let i = 0; i < cashRegisterState.length; i++) {
+    let nameInCash = cashRegisterState[i][0];
+    if (nameInCash === moneyRequestedName) {
+      return cashRegisterState[i][1];
+    }
+
+  }
+  return "NO SUCH MONIES";
+}
+
 function checkCashRegister(price, cash, cid) {
-    let change;
-    return change;
+
+    // return from biggest to smallest 
+    let change = cash - price;
+    let changeSoFar = 0;
+
+    let moduloCents = {
+      "PENNY" : 1,
+      "NICKEL" : 5,
+      "DIME" : 10,
+      "QUARTER" : 25
+    }
+
+    let moduloNotes = {
+      "ONE" : 1,
+      "FIVE" : 5,
+      "TEN" : 10,
+      "TWENTY" : 20,
+      "ONE HUNDRED" : 100,
+    }
+
+    // split to cents and dollars
+
+    for (let i = cid.length -1 ; i >= 0; i--) {
+      let moneyType = cid[i][0]
+      let moneyValue = cid[i][0]
+
+      // does change fit in the current value
+      if ()
+
+    }
+
+
+    // while (change > changeSoFar) {
+    //   // check if it is divisible by money-type
+    //   // use that money-type to pay some 
+    // }
+
+    console.log(fetchType("ONE HUNDRED", cid) )
+
+
+    if (changeSoFar === change) {
+      return {}
+    } else {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+
+
+
+
+
+
 }
 
 
-
+console.log(checkCashRegister(19.5, 20, 
+  [["PENNY", 1.01],
+   ["NICKEL", 2.05],
+   ["DIME", 3.1],
+   ["QUARTER", 4.25],
+   ["ONE", 90],
+   ["FIVE", 55],
+   ["TEN", 20],
+   ["TWENTY", 60],
+   ["ONE HUNDRED", 100]]))
 
 
 // TESTS:
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+/*
+checkCashRegister(19.5, 20,[["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 // => should return an object.
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 // => should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
@@ -68,3 +137,4 @@ checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUAR
 // => should return {status: "INSUFFICIENT_FUNDS", change: []}.
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 // => should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+*/
