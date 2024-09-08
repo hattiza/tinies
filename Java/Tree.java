09693e63b54ec61d75d7fcd7cc851bd3b66581e2
hattiza <quiet.iron8712@fastmail.com>
2024-09-08 10:53:05 -0700

    Add tree

---
+++ Java/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args){
+    // Introducing Kelli D.
+    System.out.println("Thou shall hattiza name me");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+  }
+}
