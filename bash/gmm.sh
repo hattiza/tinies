e0b0ed902292776fe9257e4c4745bcc8ef6b9e19
hattiza <quiet.iron8712@fastmail.com>
2024-09-30 11:03:25 -0700

    Add comment to what it does/usage

--- bash/gmm.sh
+++ bash/gmm.sh
@@ -8,3 +8,27 @@ for arg in "$@"; do
     substr(prevline,4,17) " " substr($0,1,2) " "; } while (1) }';
     echo '';
 done
+
+
+# USAGE
+
+# ➜  tinies git:(main) ./bash/gmm.sh 10 11   
+
+#      October 2024      
+#  Mo Tu We Th Fr Sa Su
+#      1  2  3  4  5  6 
+#   7  8  9 10 11 12 13 
+#  14 15 16 17 18 19 20 
+#  21 22 23 24 25 26 27 
+#  28 29 30 31          
+
+
+#     November 2024      
+#  Mo Tu We Th Fr Sa Su
+#               1  2  3 
+#   4  5  6  7  8  9 10 
+#  11 12 13 14 15 16 17 
+#  18 19 20 21 22 23 24 
+#  25 26 27 28 29 30    
+
+# ➜  tinies git:(main) 
